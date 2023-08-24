488ccfb494ead036fc1c5b7d86c27ebdbba82e98
Noah Nelson <noah.james.nelson@gmail.com>
2023-08-24 19:13:59 -0400

    reroutes and added controllers

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const user = require("./user");
+const reaction = require("./reaction");
+const thoughts = require("./thoughts");
+
+module.exports = { user, reaction, thoughts };
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+
+// User schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must have the format of an email!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Increases friend count in User model object when friends are added by a user
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Creates User model with userSchema
+const User = model("user", userSchema);
+
+// Exports
+module.exports = User;
