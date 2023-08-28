98e48cda135c165bf4c593381c8aaa2827759c26
Noah Nelson <noah.james.nelson@gmail.com>
2023-08-28 15:31:50 -0400

    fixing config and dependancies

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const { connect, connection } = require("mongoose");
 
-process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/friendsAndThoughtsDB";
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/friendsAndThoughtsDB";
 
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = connection;
 module.exports = connection;
